b49ae8973e34b6227dce29d59e4cc46ab4bf947d
Lance Aeby <Lance.Aeby@Renaissance.com>
2018-12-21 10:30:25 -0600

    Logging, more comments, formatting updates, wand.image testing.

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import click, datetime
+import click, datetime, logging
 from casters import casters
+from wand.image import Image
 
 @click.command()
 @click.option('--date', '-d', help='The date of the match (x/x/xxxx)')
@@ -11,6 +12,7 @@ from casters import casters
     # Pull prebuilt image of caster by name
 
 def setup(date, time, casterone, castertwo):
+    appSetup()
 
     # Validate input
     p1 = casters.get(casterone)
@@ -20,22 +22,26 @@ def setup(date, time, casterone, castertwo):
 
     if (p1 and p2):
         print('\r')
-        print('-------------------')
-        print('- Graphic Builder -')
-        print('-------------------')
+        print('---------------------------------------------------------')
+        print('-------------------- Graphic Builder --------------------')
+        print('---------------------------------------------------------')
         print('\r')
         print('Match will be on ' + formattedMatchDate + ' starting at ' + time + ' CST')
         print(p1 + ' vs ' + p2)
     else:
-        print('You dun goofed. Type better!')
+        logging.error('Not able to find one or both of the casters passed in.')
         exit()
 
     # Use valid keys to get data
-
+        # getCasterImage(casterone)
+        # getCasterImage(castertwo)
     # Build image
-
+        #with Image(width=200, height=100) as img:
+            #img.save(filename='200x100-transparent.png')
     # Return image
 
+def appSetup():
+    logging.basicConfig(format='[%(levelname)s] %(asctime)s - %(message)s', datefmt='%d-%b-%y %H:%M:%S')
     
 
 if __name__ == '__main__':
